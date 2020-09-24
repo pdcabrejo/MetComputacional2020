9c35e9d0e5c9d22c40b2e9d2d0d7760076b84ffb
asegura4488 <asegura4488@gmail.com>
2020-09-24 13:11:53 -0500

    Pre codigo orbita

---
+++ Week7/Orbita_Tierra/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+
+
+double G = (6.67e-11*pow(6.68e-12,3))/( 5e-31*pow(1.15e-5,2) );
+double Ms = 1.;
+
+int main(int argc, char *argv[]){
+
+
+
+ return 0;
+
+}
+
